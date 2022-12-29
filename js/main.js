3d66de9e26df21e9e221c98a9577615579a66349
Feterson1 <Feterson@bk.ru>
2022-12-29 15:36:36 +0600

    header nav burger

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ window.onscroll = () =>{
     }
 }
 }())
+
+// Burger handler
+
+(function(){
+    const burgerItem = document.querySelector('.burger');
+    const headerMenu = document.querySelector('.header__nav');
+    const headerClose = document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click', ()=>{
+       headerMenu.classList.add('header__nav_active');
+    })
+    headerClose.addEventListener('click', () => {
+        headerMenu.classList.remove('header__nav_active');
+    })
+
+}());
+
